de2756fa86a8e9c5adc751ad50dc68a20382bf21
Dominik Hladík <dominik.hladik@coex.cz>
2022-12-14 23:21:56 +0100

    feat(version-builder): version builder enchnaced

---
+++ README.md
@@ -0,0 +1,28 @@
+<p align="center">
+  <a href="https://github.com/Celtian/ngx-devkit" target="blank"><img src="assets/logo.svg?sanitize=true" alt="" width="120"></a>
+  <h1 align="center">NgxDevkit</h1>
+</p>
+
+[![npm version](https://badge.fury.io/js/ngx-devkit.svg)](https://badge.fury.io/js/ngx-devkit)
+[![Package License](https://img.shields.io/npm/l/ngx-devkit.svg)](https://www.npmjs.com/ngx-devkit)
+[![NPM Downloads](https://img.shields.io/npm/dm/ngx-devkit.svg)](https://www.npmjs.com/ngx-devkit)
+[![Build & Publish](https://github.com/celtian/ngx-devkit/workflows/Build%20&%20Publish/badge.svg)](https://github.com/celtian/ngx-devkit/actions)
+[![stars](https://badgen.net/github/stars/celtian/ngx-devkit)](https://github.com/celtian/ngx-devkit/)
+[![forks](https://badgen.net/github/forks/celtian/ngx-devkit)](https://github.com/celtian/ngx-devkit/)
+[![HitCount](http://hits.dwyl.com/celtian/ngx-devkit.svg)](http://hits.dwyl.com/celtian/ngx-devkit)
+
+This package contains Architect builders used to build and test Angular applications and libraries.
+
+## Builders
+
+| Name    | Description                   |
+| ------- | ----------------------------- |
+| version | Create build information file |
+
+## License
+
+Copyright &copy; 2022 [Dominik Hladik](https://github.com/Celtian)
+
+All contents are licensed under the [MIT license].
+
+[mit license]: LICENSE
--- src/version/index.ts
+++ src/version/index.ts
@@ -1,24 +1,65 @@
 import { createBuilder } from '@angular-devkit/architect';
-import { getSystemPath, normalize } from '@angular-devkit/core';
+import { getSystemPath, JsonObject, normalize } from '@angular-devkit/core';
 import { readFileSync, writeFileSync } from 'fs';
 
-export default createBuilder((options, ctx) => {
-  ctx.logger.info('Creating version');
+export type VersionField = 'version' | 'date' | 'author' | 'git';
+export type VersionLint = 'eslint' | 'tslint';
+
+export interface VersionBuilderOptions extends JsonObject {
+  outputFile: string;
+  fields: VersionField[];
+  lint: VersionLint;
+}
+
+export default createBuilder(({ outputFile, fields, lint }: VersionBuilderOptions, ctx) => {
+  ctx.logger.info('Creating version information file');
   try {
-    const fileToPatch = `${getSystemPath(normalize(ctx.workspaceRoot))}/src/assets/build.json`;
-    const packageJsonContent = readFileSync(`${getSystemPath(normalize(ctx.workspaceRoot))}/package.json`, 'utf-8');
+    const rootPath = getSystemPath(normalize(ctx.workspaceRoot));
+    const encoding: BufferEncoding = 'utf-8';
+    const fileToPatch = `${rootPath}/${outputFile}`;
+    const packageJsonContent = readFileSync(`${rootPath}/package.json`, encoding);
+    let head = readFileSync(`${rootPath}/.git/HEAD`, encoding).toString().trim();
+    if (head.split(':').length > 1) {
+      head = head.split(':')[1].trim();
+    }
+    const commit = head ? readFileSync(`${rootPath}/.git/${head}`, encoding).toString().trim() : undefined;
+    const branch = head?.split('/')?.pop();
+    const packageJson = JSON.parse(packageJsonContent);
+    const git = branch || commit ? { branch, commit } : undefined;
 
-    writeFileSync(fileToPatch, JSON.stringify({
-      version: JSON.parse(packageJsonContent).version,
-      date: new Date().toISOString(),
-    }));
+    const json = JSON.stringify(
+      {
+        version: fields.includes('version') ? packageJson.version : undefined,
+        date: fields.includes('date') ? new Date().toISOString() : undefined,
+        author: fields.includes('author') ? packageJson.author : undefined,
+        git: fields.includes('git') ? git : undefined
+      },
+      null,
+      2
+    );
 
+    const rawFormat = outputFile?.split('.')?.pop();
+    const format = ['json', 'ts'].includes(rawFormat) ? rawFormat : 'json';
+
+    if (format === 'ts') {
+      writeFileSync(
+        fileToPatch,
+        `// IMPORTANT: THIS FILE IS AUTO GENERATED!
+/* ${lint === 'tslint' ? 'tslint:disable' : 'eslint-disable'} */
+export const VERSION = ${json};
+/* ${lint === 'tslint' ? 'tslint:enable' : 'eslint-enable'} */
+`,
+        { encoding: 'utf-8' }
+      );
+    } else {
+      writeFileSync(fileToPatch, json, { encoding });
+    }
   } catch (error) {
     return {
-      success: false,
+      success: false
     };
   }
   return {
-    success: true,
+    success: true
   };
 });
